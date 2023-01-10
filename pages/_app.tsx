3d63b46e463ad48380dea23ef5cfd89ba89a2067
Balssh <george.bals25@gmail.com>
2023-01-10 12:34:40 +0200

    Minimum working setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <div className="grid grid-cols-12 px-5 my-5">
+            {/* Header */}
+            <div className="col-span-8 col-start-3 border border-zinc-900">
+                <Component {...pageProps} />
+            </div>
+            {/* Main thing */}
+        </div>
+    );
 }
