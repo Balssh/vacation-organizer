a6dbbafbdd6240bdbf5bdb44aba0a0d5ec22663b
Balssh <george.bals25@gmail.com>
2023-01-12 22:49:55 +0200

    basic user authetication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
 
 // This should be the landing page
 const Home = () => {
+  const auth = getAuth();
+  const [user, loading] = useAuthState(auth);
   return (
     <div className="my-5 grid grid-cols-12 content-evenly gap-2 px-5">
       <div className="col-span-8 col-start-3 flex items-center justify-center rounded-md border border-zinc-900 bg-teal-50">
@@ -9,9 +13,18 @@ const Home = () => {
       </div>
       <div className="col-span-8 col-start-3 space-y-2 rounded-md border border-zinc-900 bg-teal-50 p-4">
         <div className="flex justify-center">
-          <button className="border border-zinc-900 p-2 text-2xl hover:bg-red-400">
-            <Link href={"/login"}>Login</Link>
-          </button>
+          {user ? (
+            <button
+              className="border border-zinc-900 p-2 text-2xl hover:bg-red-400"
+              onClick={() => auth.signOut()}
+            >
+              Sign out
+            </button>
+          ) : (
+            <button className="border border-zinc-900 p-2 text-2xl hover:bg-red-400">
+              <Link href={"/login"}>Login</Link>
+            </button>
+          )}
         </div>
         <div className="flex justify-center">
           <button className="border border-zinc-900 p-2 text-2xl hover:bg-red-400">
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,49 @@
+import Link from "next/link";
+import { initializeFirebase } from "../firebase/firebaseApp";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+
 const Login = () => {
-  return <div>Login</div>;
+  const app = initializeFirebase();
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (user) {
+    return <div>Pla Pizda</div>;
+  }
+  const signIn = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result.user);
+  };
+  // console.log(app);
+  return (
+    <div className="my-5 grid grid-cols-12 content-evenly gap-2 px-5">
+      <div className="col-span-8 col-start-3 flex items-center justify-center rounded-md border border-zinc-900 bg-teal-50">
+        <h5 className=" text-3xl">Login Page</h5>
+      </div>
+      <div className="col-span-8 col-start-3 space-y-2 rounded-md border border-zinc-900 bg-teal-50 p-4">
+        <div className="flex justify-center">
+          <button
+            className="border border-zinc-900 p-2 text-2xl hover:bg-red-400"
+            onClick={signIn}
+          >
+            Sign in with Google
+          </button>
+        </div>
+        <div className="flex justify-center">
+          <button className="border border-zinc-900 p-2 text-2xl hover:bg-red-400">
+            <Link href={"/register"}>Register</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
